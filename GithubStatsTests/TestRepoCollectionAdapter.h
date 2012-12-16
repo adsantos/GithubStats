433d9f288fa6bcd709c460909ec51954674b61ec
Adriana Santos <adrianasucena@gmail.com>
2012-12-16 22:42:12 +0000

    Reference the pod in the test target

---
+++ GithubStatsTests/TestRepoCollectionAdapter.h
@@ -0,0 +1,13 @@
+//
+//  TestRepoCollectionAdapter.h
+//  GithubStats
+//
+//  Created by Adriana Santos on 16/12/2012.
+//  Copyright (c) 2012 Adriana Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestRepoCollectionAdapter : NSObject
+
+@end
---
+++ GithubStatsTests/TestRepoCollectionAdapter.m
@@ -0,0 +1,13 @@
+//
+//  TestRepoCollectionAdapter.m
+//  GithubStats
+//
+//  Created by Adriana Santos on 16/12/2012.
+//  Copyright (c) 2012 Adriana Santos. All rights reserved.
+//
+
+#import "TestRepoCollectionAdapter.h"
+
+@implementation TestRepoCollectionAdapter
+
+@end
