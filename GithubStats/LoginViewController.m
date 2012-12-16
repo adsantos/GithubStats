e64a21b2bd35f5aecb94d753bbc78fecb8c159d3
Adriana Santos <adrianasucena@gmail.com>
2012-12-16 18:30:24 +0000

    Connect the login with a rest api end-point

--- GithubStats/LoginViewController.m
+++ GithubStats/LoginViewController.m
@@ -6,10 +6,14 @@
 //  Copyright (c) 2012 Adriana Santos. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "LoginViewController.h"
 #import "TextFieldCell.h"
+#import "GithubWrapperClient.h"
 
 @interface LoginViewController ()
+@property (nonatomic, weak) IBOutlet UITextField *usernameTextField;
+@property (nonatomic, weak) IBOutlet UITextField *passwordTextField;
 
 @end
 
@@ -28,7 +32,11 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(dismissViewControllerAnimated:completion:)];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(dismiss)];
+    [self.button setBackgroundColor:[UIColor darkGrayColor]];
+    self.button.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    self.button.layer.borderWidth = 0.5f;
+    self.button.layer.cornerRadius = 10.0f;
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,8 +45,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)dismiss {
+//    [super dismissViewControllerAnimated:flag completion:completion];
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
 -(IBAction)buttonTapped:(id)sender {
 //    [self.button.titleLabel setEnabled:NO];
+    [[GithubWrapperClient sharedInstance] validUsername:self.usernameTextField.text andPassword:self.passwordTextField.text onSuccess:^{
+        [self.presentingViewController dismissViewControllerAnimated:YES completion:^{
+            
+        }];
+    } onFailure:^(NSError *error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"An error occurred" message:[[error userInfo] objectForKey:NSLocalizedDescriptionKey] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }];
     
 }
 
@@ -76,10 +99,12 @@
     if ([indexPath row] == 0) {
         [[(TextFieldCell *)cell textField] setSecureTextEntry:NO];
         [[(TextFieldCell *)cell textField] setPlaceholder:@"github username"];
+        self.usernameTextField = [(TextFieldCell *)cell textField];
     }
     else {
         [[(TextFieldCell *)cell textField] setSecureTextEntry:YES];
-        [[(TextFieldCell *)cell textField] setPlaceholder:@"password"];
+        [[(TextFieldCell *)cell textField] setPlaceholder:@"github password"];
+        self.passwordTextField = [(TextFieldCell *)cell textField];
     }
     
     return cell;
